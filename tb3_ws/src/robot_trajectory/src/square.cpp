1c8f1eae37ff8ce26f0a395149b81fdbcc4960ac
Dani <danielgarciagranada03@gmail.com>
2025-06-26 20:55:45 +0200

    move forward 1m and turn 90º

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -14,12 +14,25 @@ int main(int argc, char * argv[])
     rclcpp::WallRate loop_rate(10ms);
 
     int i=0, n=1000; 
-    while(rclcpp::ok() && i<n) {
+    while(rclcpp::ok() && i<n)
+    {
         i++;
         message.linear.x = 0.1;
         rclcpp::spin_some(node);
         loop_rate.sleep();
     }
+    
+    int j = 0, n_turn = 1011;
+    while(rclcpp::ok() && j < n_turn)
+    {
+       j++;
+       message.linear.x = 0.0;
+       message.angular.z = -(9*M_PI/180);
+       publisher->publish(message);
+       rclcpp::spin_some(node);
+       loop_rate.sleep();
+    }
+    
     //send zero velocity to topic
     message.linear.x = 0.0;
     message.angular.z = 0.0;
