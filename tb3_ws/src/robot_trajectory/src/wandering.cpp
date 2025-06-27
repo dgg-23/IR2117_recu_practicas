35177a5ee1b3654588ca1974ed62d2ad68c05219
Dani <danielgarciagranada03@gmail.com>
2025-06-27 22:08:32 +0200

    wandering v2

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -15,11 +15,10 @@ int main(int argc, char * argv[])
     message_cmd_vel.data = "0";
     auto subscription_scan = node->create_subscription<sensor_msgs::msg::LaserScan>("/scan", 10, [&](const sensor_msgs::msg::LaserScan::SharedPtr msg)
     {
-        for (auto range : msg->ranges)
-        {
-            std::cout << range << " ";
-        }
-        std::cout << std::endl;
+        std::cout << "[0] " << msg->ranges[0] << std::endl;
+        std::cout << "[90] " << msg->ranges[msg->ranges.size() / 4] << std::endl;
+        std::cout << "[180] " << msg->ranges[msg->ranges.size() / 2] << std::endl;
+        std::cout << "[270] " << msg->ranges[(msg->ranges.size() * 3) / 4] << std::endl;
     });
 
     rclcpp::WallRate loop_rate(10ms);
