85cf683535f49e5c538a0b587a05f6d7180ca53e
Dani <danielgarciagranada03@gmail.com>
2025-06-28 18:20:14 +0200

    Implement FSA - states and transitions
    sale raro los parentesis porque lo edite al principio en el bloc de ntoas y luego para hacerlo bien me pues en el visual studio y por eso sale asi. Si se abre en visual studio se ve bien

--- tb3_ws/src/obstacles/src/avoidance.cpp
+++ tb3_ws/src/obstacles/src/avoidance.cpp
@@ -1,9 +1,20 @@
 #include <chrono>
+#include <cstdlib>
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "example_interfaces/msg/bool.hpp"
+#include <random>
 
 using namespace std::chrono_literals;
+
+enum State
+{
+    FORWARD,
+    TURN_LEFT,
+    TURN_RIGHT
+};
+
+State state = FORWARD; //estado inicial
 
 bool front_obstacle = false;
 bool left_obstacle = false;
@@ -34,11 +45,64 @@ int main(int argc, char * argv[])
     auto subs_right = node->create_subscription<example_interfaces::msg::Bool>("/right/obstacle", 10, callback_right);
 	geometry_msgs::msg::Twist message;
 	rclcpp::WallRate loop_rate(50ms);
-	while (rclcpp::ok()) {
-		publisher->publish(message);
-		rclcpp::spin_some(node);
-		loop_rate.sleep();
-	}
+
+	while (rclcpp::ok())
+    {
+		switch (state)
+        {
+            case FORWARD:
+                message.linear.x = 0.5;
+                message.angular.z = 0.0;
+                if (front_obstacle)
+                {
+                    if (left_obstacle && !right_obstacle) 
+                    {
+                        state = TURN_RIGHT;
+                    } 
+                    else if (right_obstacle && !left_obstacle) 
+                    {
+                        state = TURN_LEFT;
+                    }
+                    else
+                    {
+                        // Tria aleatoriamente si girar der o izq
+                        if (rand() >= 0.5)
+                        {
+                            state = TURN_LEFT;
+                        }
+                        else
+                        {
+                            state = TURN_RIGHT;
+                        }
+                    }
+                }
+                break;
+                
+            case TURN_LEFT:
+                message.linear.x = 0.0;
+                message.angular.z = 0.5;
+                if (!front_obstacle)
+                {
+                    state = FORWARD;
+                }
+                break;
+
+            case TURN_RIGHT:
+                message.linear.x = 0.0;
+                message.angular.z = -0.5;
+                if (!front_obstacle)
+                {
+                    state = FORWARD;
+
+                }
+                break;
+        }
+
+        publisher->publish(message);
+        rclcpp::spin_some(node);
+        loop_rate.sleep();
+    }
+
 	rclcpp::shutdown();
 	return 0;
 }
