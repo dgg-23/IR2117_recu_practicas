ba139f5ac63121593fc53333de5d1066657bd89c
Dani <danielgarciagranada03@gmail.com>
2025-06-28 03:35:55 +0200

    wandering v7
    giramos hacia el lado con la mayor distancia minima

--- tb3_ws/src/robot_trajectory/src/wandering.cpp
+++ tb3_ws/src/robot_trajectory/src/wandering.cpp
@@ -2,8 +2,36 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 using namespace std::chrono_literals;
+
+float min_range_l = std::numeric_limits<float>::max();
+float min_range_d = std::numeric_limits<float>::max();
+
+void scanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg) //min ranges
+{
+    min_range_l = std::numeric_limits<float>::max(); //min ranges[0..9]
+    for (int i = 0; i < 10; ++i) {
+        if (msg->ranges[i] < min_range_l)
+        {
+            min_range_l = msg->ranges[i];
+        }
+    }
+
+    min_range_r = std::numeric_limits<float>::max(); //min ranges[350..359]
+
+    for (int i = 350; i < 360; ++i)
+    {
+        if (msg->ranges[i] < min_range_r)
+        {
+            min_range_r = msg->ranges[i];
+        }
+    }
+
+    std::cout << "Mínimo [0..9]: " << min_range_l << std::endl;
+    std::cout << "Mínimo [350..359]: " << min_range_r << std::endl;
+}
 
 int main(int argc, char * argv[])
 {
